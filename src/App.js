0bb5806a39b7a148146183a647530648ffd0de14
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2021-12-13 16:36:50 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 // import "./styles.css";
 import { CssModules } from "./components/CssModules";
+import { StyledJsx } from "./components/StyledJsx";
+import { StyledComponents } from "./components/StyledComponents";
 
 export default function App() {
   return (
     <div>
       <CssModules />
+      <StyledJsx />
+      <StyledComponents />
     </div>
   );
 }
---
+++ src/components/Styled.jsx
@@ -0,0 +1,3 @@
+export const StyledJsx = () => {
+  return <div></div>;
+};
